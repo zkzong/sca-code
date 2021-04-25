4d9c4e9650ad6b4d912e8e8e1fd195eeb75ab7e7
zongzhankui <zongzhankui@hotmail.com>
2021-04-25 09:44:38 +0800

    sentinel

--- 12-config/order-service/src/main/java/com/lagou/orderservice/controller/TestController.java
+++ 12-config/order-service/src/main/java/com/lagou/orderservice/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.lagou.orderservice.controller;
 
 import com.lagou.orderservice.config.CustomConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +12,7 @@ public class TestController {
     private CustomConfig customConfig;
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "flag:" + customConfig.getFlag() + "<br/> database:" + customConfig.getDatabase();
     }
 }
