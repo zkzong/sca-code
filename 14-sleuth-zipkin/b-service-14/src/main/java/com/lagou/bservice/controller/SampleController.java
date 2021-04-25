d77745230b911983a29a4058eb5b4f71f469795a
zongzhankui <zongzhankui@hotmail.com>
2021-04-25 09:52:14 +0800

    sleuth zipkin

---
+++ 14-sleuth-zipkin/b-service-14/src/main/java/com/lagou/bservice/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.lagou.bservice.controller;
+
+import com.lagou.bservice.feignclient.CServiceFeignClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+public class SampleController {
+    @Resource
+    private CServiceFeignClient cService;
+    @GetMapping("/b")
+    @ResponseBody
+    public String methodB(){
+        String result = cService.methodC();
+        result = " -> Service B" + result;
+        return result;
+    }
+}
