ca2af22bd4e111befbf9bbbc38a31b951ab0110a
zongzhankui <zongzhankui@hotmail.com>
2021-05-02 17:52:04 +0800

    config

--- 12-config/order-service/src/main/java/com/lagou/orderservice/controller/TestController.java
+++ 12-config/order-service/src/main/java/com/lagou/orderservice/controller/TestController.java
@@ -1,18 +1,24 @@
 package com.lagou.orderservice.controller;
 
 import com.lagou.orderservice.config.CustomConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class TestController {
     @Resource
     private CustomConfig customConfig;
 
+    @Value("${custom.aaa}")
+    private String aaa;
+
     @GetMapping("/test")
     public String test() {
-        return "flag:" + customConfig.getFlag() + "<br/> database:" + customConfig.getDatabase();
+        return "flag:" + customConfig.getFlag() + "<br/> database:" + customConfig.getDatabase() + " " + aaa;
     }
 }
